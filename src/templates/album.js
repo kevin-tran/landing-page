c6429f19e9e62ddf456ff38d547b04ccb0c9b737
kevin <thebounstar@gmail.com>
2019-03-06 20:26:52 +1100

    seo info for album template

--- src/templates/album.js
+++ src/templates/album.js
@@ -2,8 +2,10 @@ import React from "react";
 import { graphql } from "gatsby";
 import Img from "gatsby-image";
 import styled from "@emotion/styled";
+import get from "lodash.get";
 
 import Layout from "../components/layout/layout";
+import SEO from "../components/seo/seo";
 
 const Gutter = styled("section")({
   margin: "0 auto"
@@ -26,10 +28,11 @@ const NonStretchedImage = props => {
 };
 
 const AlbumTemplate = ({ data }) => {
-  const album = data.contentfulAlbum.images;
-
+  const album = get(data, "contentfulAlbum.images");
+  const title = get(data, "contentfulAlbum.title");
   return (
     <Layout>
+      <SEO title={title} />
       <Gutter>
         {album.map(image => {
           return <NonStretchedImage {...image} />;
@@ -44,6 +47,7 @@ export default AlbumTemplate;
 export const pageQuery = graphql`
   query($id: String!) {
     contentfulAlbum(id: { eq: $id }) {
+      title
       images {
         fluid(maxWidth: 1306, quality: 80) {
           base64
